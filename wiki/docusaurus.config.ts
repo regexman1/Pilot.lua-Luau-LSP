6b13a8e785bf90fe317088e7d574df76053cb0dc
regexman1 <124459272+regexman1@users.noreply.github.com>
2025-03-07 12:26:21 +0200

    Update docusaurus.config.ts

--- wiki/docusaurus.config.ts
+++ wiki/docusaurus.config.ts
@@ -125,6 +125,13 @@ const config: Config = {
 				defaultMode: "dark",
 				respectPrefersColorScheme: true,
 			},
+			announcementBar: {
+				id: "find_outdated_info",
+				content: "Report any undocumented and outdated info by opening an issue.",
+				backgroundColor: '#fafbfc',
+        textColor: '#091E42',
+        isCloseable: false,
+			}
 		},
 	} satisfies Preset.ThemeConfig,
 };
